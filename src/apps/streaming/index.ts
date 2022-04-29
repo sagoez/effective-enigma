d386ac03ce59fd328913dbe54986091541cd5f82
samgj18 <samgj_18@hotmail.com>
2022-04-29 23:58:26 +0100

    Hotfix: making 404 lazy to be able to deploy

--- src/apps/streaming/index.ts
+++ src/apps/streaming/index.ts
@@ -2,13 +2,14 @@ import * as T from "@effect-ts/core/Effect"
 import { pipe } from "@effect-ts/core/Function"
 import * as App from "./routes"
 
-const redirectTo404 = T.succeed(
-  new Response("404 Not Found", {
-    status: 404,
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  }),
+const redirectTo404 = T.succeedWith(
+  () =>
+    new Response("404 Not Found", {
+      status: 404,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }),
 )
 
 export const program = pipe(
